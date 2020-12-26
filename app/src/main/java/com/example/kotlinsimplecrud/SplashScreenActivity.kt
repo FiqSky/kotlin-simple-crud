43445d983d508376e901531b7359661fc67fd91d
FiqSky <fiqihfm05@gmail.com>
2020-12-26 11:40:55 +0700

    change group & add SplashScreenActivity.kt

---
+++ app/src/main/java/com/example/kotlinsimplecrud/SplashScreenActivity.kt
@@ -0,0 +1,20 @@
+package com.example.kotlinsimplecrud
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+@Suppress("DEPRECATION")
+class SplashScreenActivity : AppCompatActivity() {
+    private val timeOut : Long = 2000 //2 sec
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, timeOut)
+    }
+}
